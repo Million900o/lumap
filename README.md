9e39c030debef28f4a7c974e67809ba388e35b40
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-02-10 22:39:42 +0700

    lol

---
+++ README.md
@@ -0,0 +1,12 @@
+# Lumap
+
+```
+$ npm i lumap
+```
+
+```js
+const Lumap = require('lumap');
+const lumap = new Lumap();
+
+lumap.cute() // true
+```
---
+++ index.js
@@ -0,0 +1,12 @@
+class Lu extends Map {
+  constructor(opt) {
+    super(opt);
+    this.qt = true;  
+  }
+  
+  cute() {
+    return this.cute;
+  }
+}
+
+module.exports = Lu;
